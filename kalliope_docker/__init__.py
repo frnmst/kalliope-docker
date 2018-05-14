495df526857d2bbb03306c1ae701ffc2ffa3f6be
Franco Masotti <franco.masotti@live.com>
2018-05-14 16:26:35 +0200

    Fixed exports.

--- kalliope_docker/__init__.py
+++ kalliope_docker/__init__.py
@@ -1,3 +1,3 @@
-from .core import (generate_dockerfile, build_shell_command,
-                   execute_shell_command, get_git_repository_name_from_url,
-                   load_yaml_file, profile_pipeline, load_standard_packages_from_files)
+from .core import (generate_dockerfile, profile_pipeline,
+                   load_standard_packages_from_files, load_configuration_file,
+                   write_dockerfile, install_profile)
