e4ef7eb1fcbd5615e63722bf83b26acd058221cc
Franco Masotti <franco.masotti@live.com>
2018-06-18 18:07:23 +0200

    Added documentation.

--- kalliope_docker/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import (quote_for_shell, get_git_repository_name_from_url,
-                   generate_dockerfile, profile_pipeline,
-                   load_standard_packages_from_files, load_configuration_file,
-                   write_dockerfile, build_docker_image, run_docker_container,
-                   clear_cache, stop_docker_container)
